cb98d7dd64ff41a43ca3dd2e209080c0663d391e
trieu <trieu@astech-solutions.com>
2026-09-21 09:55:25 +0200

    210926 - données fixes/ style fixe + ajout d'un component catList et catCard

--- app/catpack/page.tsx
+++ app/catpack/page.tsx
@@ -1,3 +1,8 @@
+import CatList from '@/app/catpack/_component/catList';
+
 export default function CatPackPage() {
-  return <main>CatPack</main>;
+  return <main>
+   <h1>CatPack</h1>
+   <CatList/>
+  </main>;
 }
--- app/utils/router.tsx
+++
@@ -1,3 +0,0 @@
-function Router() {
-    return
-}
